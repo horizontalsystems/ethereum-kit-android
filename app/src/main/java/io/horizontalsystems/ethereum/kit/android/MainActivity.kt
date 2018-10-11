8d54b09108503fafe0f3767f610862ca3f1ee955
chyngyz <chyngyz.omurov@gmail.com>
2018-10-11 11:41:45 +0600

    Create initial empty project structure

---
+++ app/src/main/java/io/horizontalsystems/ethereum/kit/android/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.horizontalsystems.ethereum.kit.android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
