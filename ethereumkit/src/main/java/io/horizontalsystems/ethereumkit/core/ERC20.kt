46c3caf695959877b12f54089ea1fff8d685ad01
Talgat Medetbekov <medetbekov.t@gmail.com>
2019-01-31 11:01:15 +0600

    Track "kit state" separately for every token

--- ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/core/ERC20.kt
+++ ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/core/ERC20.kt
@@ -1,5 +1,15 @@
 package io.horizontalsystems.ethereumkit.core
 
 import io.horizontalsystems.ethereumkit.EthereumKit
+import io.horizontalsystems.ethereumkit.EthereumKit.KitState
 
-class ERC20(var listener: EthereumKit.ListenerERC20?, var balance: Double = 0.0)
+class ERC20(var listener: EthereumKit.ListenerERC20) {
+
+    var balance: Double = 0.0
+
+    var kitState: KitState = KitState.NotSynced
+        set(value) {
+            listener.onKitStateUpdate(value)
+            field = value
+        }
+}
