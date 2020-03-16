aa1a23a1eaa4d4c476f1cf1090cf384e2de5b6ef
chyngyz <chyngyz.omurov@gmail.com>
2020-03-16 11:54:33 +0600

    Fix bugs in IncubedRpcApiProvider
    - in transactionReceiptStatus return correct TransactionStatus
    - rename execute function to serialExecute and use it for all methods to execute RPC calls serially

--- ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/core/IncubedRpcApiProvider.kt
+++ ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/core/IncubedRpcApiProvider.kt
@@ -28,7 +28,7 @@ class IncubedRpcApiProvider(
             else -> Chain.KOVAN
         }.let { chain ->
             val in3Instance = IN3.forChain(chain)
-            in3Instance.proof = Proof.none
+            in3Instance.config.proof = Proof.none
             in3Instance
         }
     }
@@ -36,15 +36,8 @@ class IncubedRpcApiProvider(
     private val pool = Executors.newFixedThreadPool(1)
 
     @Synchronized
-    private fun <T> execute(block: () -> T): T {
+    private fun <T> serialExecute(block: () -> T): T {
         return pool.submit(block).get()
-
-//        return Single.fromCallable {
-//            block.invoke()
-//        }.unsubscribeOn(scheduler)
-//                .subscribeOn(scheduler)
-//                .observeOn(scheduler)
-//                .blockingGet()
     }
 
     override val source: String
@@ -53,52 +46,67 @@ class IncubedRpcApiProvider(
     override fun getLastBlockHeight(): Single<Long> {
         logger.info("IncubedRpcApiProvider: getLastBlockHeight")
         return Single.fromCallable {
-            execute { eth1.blockNumber }
+            serialExecute {
+                eth1.blockNumber
+            }
         }
     }
 
     override fun getTransactionCount(): Single<Long> {
         logger.info("IncubedRpcApiProvider: getTransactionCount")
-        return Single.fromCallable { eth1.getTransactionCount(address.toHexString(), LATEST).toLong() }
+        return Single.fromCallable {
+            serialExecute {
+                eth1.getTransactionCount(address.toHexString(), LATEST).toLong()
+            }
+        }
     }
 
     override fun getBalance(): Single<BigInteger> {
         logger.info("IncubedRpcApiProvider: getBalance")
         return Single.fromCallable {
-            execute { eth1.getBalance(address.toHexString(), LATEST) }
+            serialExecute {
+                eth1.getBalance(address.toHexString(), LATEST)
+            }
         }
     }
 
     override fun send(signedTransaction: ByteArray): Single<Unit> {
         logger.info("IncubedRpcApiProvider: send")
-        return Single.fromCallable { eth1.sendRawTransaction(signedTransaction.toHexString()) }.map { Unit }
+        return Single.fromCallable {
+            serialExecute {
+                eth1.sendRawTransaction(signedTransaction.toHexString())
+            }
+        }.map { Unit }
+
     }
 
     override fun getStorageAt(contractAddress: ByteArray, position: String, blockNumber: Long?): Single<String> {
         logger.info("IncubedRpcApiProvider: getStorageAt")
         return Single.fromCallable {
-            eth1.getStorageAt(contractAddress.toHexString(), position.toBigInteger(), blockNumber
-                    ?: LATEST)
+            serialExecute {
+                eth1.getStorageAt(contractAddress.toHexString(), position.toBigInteger(), blockNumber
+                        ?: LATEST)
+            }
         }
     }
 
     override fun getLogs(address: ByteArray?, fromBlock: Long, toBlock: Long, topics: List<ByteArray?>): Single<List<EthereumLog>> {
         logger.info("IncubedRpcApiProvider: getLogs")
 
-        in3.proof = Proof.none  // TODO this assignment doesn't have any effect, need to debug in in3-c client code
-
         return Single.fromCallable {
-            var requestFrom = fromBlock
-            val logs = mutableListOf<EthereumLog>()
-            while (requestFrom < toBlock) {
+            serialExecute {
+                var requestFrom = fromBlock
+                val logs = mutableListOf<EthereumLog>()
+                while (requestFrom < toBlock) {
 
-                val requestTo = if (requestFrom + GET_LOGS_REQUEST_MAX_BLOCKS_RANGE > toBlock) toBlock else requestFrom + GET_LOGS_REQUEST_MAX_BLOCKS_RANGE
-                val partialLogs = getLogsBlocking(address, requestFrom, requestTo, topics)
-                logs.addAll(partialLogs)
+                    val requestTo = if (requestFrom + GET_LOGS_REQUEST_MAX_BLOCKS_RANGE > toBlock) toBlock else requestFrom + GET_LOGS_REQUEST_MAX_BLOCKS_RANGE
+                    val partialLogs = getLogsBlocking(address, requestFrom, requestTo, topics)
+                    logs.addAll(partialLogs)
 
-                requestFrom = requestTo + 1
+                    requestFrom = requestTo + 1
+                }
+                logs
             }
-            logs
         }
     }
 
@@ -117,7 +125,11 @@ class IncubedRpcApiProvider(
 
     override fun getBlock(blockNumber: Long): Single<Block> {
         logger.info("IncubedRpcApiProvider: getBlock")
-        return Single.fromCallable { eth1.getBlockByNumber(blockNumber, false) }.map {
+        return Single.fromCallable {
+            serialExecute {
+                eth1.getBlockByNumber(blockNumber, false)
+            }
+        }.map {
             Block(it.number, it.timeStamp)
         }
     }
@@ -125,13 +137,14 @@ class IncubedRpcApiProvider(
     override fun call(contractAddress: ByteArray, data: ByteArray, blockNumber: Long?): Single<String> {
         logger.info("IncubedRpcApiProvider: call")
         return Single.fromCallable {
-            execute {
+            serialExecute {
                 val request = TransactionRequest().apply {
                     this.to = contractAddress.toHexString()
                     this.data = data.toHexString()
 
                 }
-                eth1.call(request, blockNumber ?: LATEST).toString()
+                val result = eth1.call(request, blockNumber ?: LATEST)
+                result.toString()
             }
         }
     }
@@ -139,26 +152,32 @@ class IncubedRpcApiProvider(
     override fun estimateGas(from: String?, to: String, value: BigInteger?, gasLimit: Long?, gasPrice: Long?, data: String?): Single<String> {
         logger.info("IncubedRpcApiProvider: estimateGas")
         return Single.fromCallable {
-            val request = TransactionRequest().apply {
-                from?.let { this.from = it }
-                this.to = to
-                value?.let { this.value = it }
-                gasLimit?.let { this.gas = it }
-                gasPrice?.let { this.gasPrice = it }
-                data?.let { this.data = it }
+            serialExecute {
+                val request = TransactionRequest().apply {
+                    from?.let { this.from = it }
+                    this.to = to
+                    value?.let { this.value = it }
+                    gasLimit?.let { this.gas = it }
+                    gasPrice?.let { this.gasPrice = it }
+                    data?.let { this.data = it }
+                }
+                eth1.estimateGas(request, LATEST).toString()
             }
-            eth1.estimateGas(request, LATEST).toString()
         }
     }
 
     override fun transactionReceiptStatus(transactionHash: ByteArray): Single<TransactionStatus> {
-        logger.info("IncubedRpcApiProvider: transactionReceiptStatus")
+        logger.info("IncubedRpcApiProvider: transactionReceiptStatus: ${transactionHash.toHexString()}")
         return Single.fromCallable {
-            val receipt = eth1.getTransactionReceipt(transactionHash.toHexString())
-            if (receipt.status) {
-                TransactionStatus.FAILED
-            } else {
-                TransactionStatus.SUCCESS
+            serialExecute {
+                val receipt = eth1.getTransactionReceipt(transactionHash.toHexString())
+                logger.info("IncubedRpcApiProvider: transactionReceiptStatus receipt: ${receipt.status}")
+
+                if (receipt.status) {
+                    TransactionStatus.SUCCESS
+                } else {
+                    TransactionStatus.FAILED
+                }
             }
         }
     }
@@ -166,8 +185,10 @@ class IncubedRpcApiProvider(
     override fun transactionExist(transactionHash: ByteArray): Single<Boolean> {
         logger.info("IncubedRpcApiProvider: transactionExist")
         return Single.fromCallable {
-            val tx = eth1.getTransactionByHash(transactionHash.toHexString())
-            tx != null
+            serialExecute {
+                val tx = eth1.getTransactionByHash(transactionHash.toHexString())
+                tx != null
+            }
         }
     }
 
