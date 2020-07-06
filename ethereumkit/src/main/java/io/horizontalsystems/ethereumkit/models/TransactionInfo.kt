decb2826fb8620242d4f01564233a3c5044c684c
chyngyz <chyngyz.omurov@gmail.com>
2020-07-06 16:43:22 +0600

    Refactor model TransactionInfo, remove unused field 'contractAddress'

--- ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/TransactionInfo.kt
+++ ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/TransactionInfo.kt
@@ -2,54 +2,23 @@ package io.horizontalsystems.ethereumkit.models
 
 import io.horizontalsystems.ethereumkit.core.toEIP55Address
 import io.horizontalsystems.ethereumkit.core.toHexString
-import io.horizontalsystems.ethereumkit.core.toRawHexString
-import io.horizontalsystems.ethereumkit.spv.core.toBigInteger
 
 class TransactionInfo(transaction: EthereumTransaction) {
-    val hash: String
-    val nonce: Long
-    val input: String
-    val from: String
-    val to: String
-    val value: String
-    val gasLimit: Long
-    val gasPrice: Long
-    val timestamp: Long
+    val hash: String = transaction.hash.toHexString()
+    val nonce: Long = transaction.nonce
+    val input: String = transaction.input.toHexString()
+    val from: String = transaction.from.toEIP55Address()
+    val to: String = transaction.to.toEIP55Address()
+    val value: String = transaction.value.toString()
+    val gasLimit: Long = transaction.gasLimit
+    val gasPrice: Long = transaction.gasPrice
+    val timestamp: Long = transaction.timestamp
 
-    val blockHash: String?
-    val blockNumber: Long?
-    val gasUsed: Long?
-    val cumulativeGasUsed: Long?
-    val isError: Int?
-    val transactionIndex: Int?
-    val txReceiptStatus: Int?
-
-    val contractAddress: String?
-
-    init {
-        hash = transaction.hash.toHexString()
-        nonce = transaction.nonce
-        input = transaction.input.toHexString()
-        from = transaction.from.toEIP55Address()
-        gasLimit = transaction.gasLimit
-        gasPrice = transaction.gasPrice
-        timestamp = transaction.timestamp
-        blockHash = transaction.blockHash?.toHexString()
-        blockNumber = transaction.blockNumber
-        gasUsed = transaction.gasUsed
-        cumulativeGasUsed = transaction.cumulativeGasUsed
-        isError = transaction.iserror
-        transactionIndex = transaction.transactionIndex
-        txReceiptStatus = transaction.txReceiptStatus
-        val data = transaction.input
-        if (data.count() == 68 && data.copyOfRange(0, 4).toRawHexString() == "a9059cbb") {
-            to = data.copyOfRange(4, 36).toEIP55Address()
-            value = data.copyOfRange(36, 68).toBigInteger().toString(10)
-            contractAddress = transaction.to.toEIP55Address()
-        } else {
-            to = transaction.to.toEIP55Address()
-            value = transaction.value.toString(10)
-            contractAddress = null
-        }
-    }
+    val blockHash: String? = transaction.blockHash?.toHexString()
+    val blockNumber: Long? = transaction.blockNumber
+    val gasUsed: Long? = transaction.gasUsed
+    val cumulativeGasUsed: Long? = transaction.cumulativeGasUsed
+    val isError: Int? = transaction.iserror
+    val transactionIndex: Int? = transaction.transactionIndex
+    val txReceiptStatus: Int? = transaction.txReceiptStatus
 }
