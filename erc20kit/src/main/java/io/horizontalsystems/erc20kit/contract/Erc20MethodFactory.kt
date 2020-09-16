473962b221a33540130120c0dad4a521f74ce47c
bakyt <abdrasulov@gmail.com>
2020-09-16 12:20:15 +0600

    Register method factories by their methodId

--- erc20kit/src/main/java/io/horizontalsystems/erc20kit/contract/Erc20MethodFactory.kt
+++ erc20kit/src/main/java/io/horizontalsystems/erc20kit/contract/Erc20MethodFactory.kt
@@ -1,6 +1,8 @@
 package io.horizontalsystems.erc20kit.contract
 
 interface Erc20MethodFactory {
+
+    val methodId: ByteArray
     fun createMethod(inputArguments: ByteArray): Erc20Method
-    fun canCreateMethod(methodId: ByteArray): Boolean
+
 }
