c1439488c3dd6e873e1e9ac898e532cbccb7d2b2
chyngyz <chyngyz.omurov@gmail.com>
2021-03-25 10:33:15 +0600

    Remove unused error, refactor ContractMethodHelper

--- ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/api/models/ApiError.kt
+++
@@ -1,8 +0,0 @@
-package io.horizontalsystems.ethereumkit.api.models
-
-import java.lang.Exception
-
-sealed class ApiError : Exception() {
-    object InvalidData : ApiError()
-    class InfuraError(code: Int, message: String) : ApiError()
-}
