769b148e3c5a5a73559e6bd01e8d2c6bac9b6062
chyngyz <chyngyz.omurov@gmail.com>
2021-04-02 10:33:49 +0600

    Initial implementation of 1inch kit

--- ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/TransactionData.kt
+++ ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/TransactionData.kt
@@ -1,9 +1,28 @@
 package io.horizontalsystems.ethereumkit.models
 
+import io.horizontalsystems.ethereumkit.core.toHexString
 import java.math.BigInteger
+import java.util.*
 
 data class TransactionData(
         val to: Address,
         val value: BigInteger,
         val input: ByteArray
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        return when {
+            this === other -> true
+            other is TransactionData -> to == other.to && value == other.value && input.contentEquals(other.input)
+            else -> false
+        }
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(to, value, input)
+    }
+
+    override fun toString(): String {
+        return "TransactionData {to: ${to.hex}, value: $value, input: ${input.toHexString()}}"
+    }
+
+}
---
+++ oneinchkit/src/main/java/io/horizontalsystems/oneinchkit/OneInchKit.kt
@@ -0,0 +1,143 @@
+package io.horizontalsystems.oneinchkit
+
+import com.google.gson.annotations.SerializedName
+import io.horizontalsystems.ethereumkit.core.EthereumKit
+import io.horizontalsystems.ethereumkit.core.EthereumKit.NetworkType
+import io.horizontalsystems.ethereumkit.core.toHexString
+import io.horizontalsystems.ethereumkit.models.Address
+import io.reactivex.Single
+import java.math.BigInteger
+import java.util.*
+
+class OneInchKit(
+        private val evmKit: EthereumKit,
+        private val service: OneInchService
+) {
+
+    val smartContractAddress: Address = when (evmKit.networkType) {
+        NetworkType.EthMainNet -> Address("0x11111112542d85b3ef69ae05771c2dccff4faa26")
+        NetworkType.BscMainNet -> Address("0x11111112542d85b3ef69ae05771c2dccff4faa26")
+        else -> throw IllegalArgumentException("Invalid NetworkType: $evmKit.networkType")
+    }
+
+    fun getApproveCallDataAsync(tokenAddress: Address, amount: BigInteger): Single<ApproveCallData> {
+        return service.getApproveCallDataAsync(tokenAddress, amount)
+    }
+
+    fun getQuoteAsync(
+            fromToken: Address,
+            toToken: Address,
+            amount: BigInteger,
+            protocols: List<String>? = null,
+            gasPrice: Long? = null,
+            complexityLevel: Int? = null,
+            connectorTokens: List<String>? = null,
+            gasLimit: Long? = null,
+            mainRouteParts: Int? = null,
+            parts: Int? = null
+    ): Single<Quote> {
+        return service.getQuoteAsync(fromToken, toToken, amount, protocols, gasPrice, complexityLevel, connectorTokens, gasLimit, mainRouteParts, parts)
+    }
+
+    fun getSwapAsync(
+            fromToken: Address,
+            toToken: Address,
+            amount: BigInteger,
+            slippagePercentage: Float,
+            protocols: List<String>? = null,
+            recipient: Address? = null,
+            gasPrice: Long? = null,
+            burnChi: Boolean = false,
+            complexityLevel: Int? = null,
+            connectorTokens: List<String>? = null,
+            allowPartialFill: Boolean = false,
+            gasLimit: Long? = null,
+            parts: Int? = null,
+            mainRouteParts: Int? = null
+    ): Single<Swap> {
+        return service.getSwapAsync(fromToken, toToken, amount, evmKit.receiveAddress, slippagePercentage, protocols, recipient, gasPrice, burnChi, complexityLevel, connectorTokens, allowPartialFill, gasLimit, parts, mainRouteParts)
+    }
+
+    companion object {
+
+        fun getInstance(evmKit: EthereumKit): OneInchKit {
+            val service = OneInchService(evmKit.networkType)
+            return OneInchKit(evmKit, service)
+        }
+
+    }
+
+}
+
+data class Token(
+        val symbol: String,
+        val name: String,
+        val decimals: Int,
+        val address: String,
+        val logoURI: String
+)
+
+data class Quote(
+        val fromToken: Token,
+        val toToken: Token,
+        val fromTokenAmount: BigInteger,
+        val toTokenAmount: BigInteger,
+        @SerializedName("protocols") val route: List<Any>,
+        val estimatedGas: Long
+) {
+    override fun toString(): String {
+        return "Quote {fromToken: ${fromToken.name}, toToken: ${toToken.name}, fromTokenAmount: $fromTokenAmount, toTokenAmount: $toTokenAmount}"
+    }
+}
+
+data class SwapTransaction(
+        val from: Address,
+        val to: Address,
+        val data: ByteArray,
+        val value: BigInteger,
+        val gasPrice: Long,
+        @SerializedName("gas") val gasLimit: Long
+) {
+    override fun toString(): String {
+        return "SwapTransaction {\nfrom: ${from.hex}, \nto: ${to.hex}, \ndata: ${data.toHexString()}, \nvalue: $value, \ngasPrice: $gasPrice, \ngasLimit: $gasLimit\n}"
+    }
+}
+
+data class Swap(
+        val fromToken: Token,
+        val toToken: Token,
+        val fromTokenAmount: BigInteger,
+        val toTokenAmount: BigInteger,
+        @SerializedName("protocols") val route: List<Any>,
+        @SerializedName("tx") val transaction: SwapTransaction
+) {
+    override fun toString(): String {
+        return "Swap {\nfromToken: ${fromToken.name}, \ntoToken: ${toToken.name}, \nfromTokenAmount: $fromTokenAmount, \ntoTokenAmount: $toTokenAmount, \ntx: $transaction\n}"
+    }
+}
+
+data class ApproveCallData(
+        val data: ByteArray,
+        val gasPrice: Long,
+        val to: Address,
+        val value: BigInteger
+) {
+    override fun equals(other: Any?): Boolean {
+        return when {
+            this === other -> true
+            other is ApproveCallData -> to == other.to && value == other.value && data.contentEquals(other.data)
+            else -> false
+        }
+    }
+
+    override fun hashCode(): Int {
+        return Objects.hash(to, value, data)
+    }
+
+    override fun toString(): String {
+        return "ApproveCallData {\nto: ${to.hex}, \nvalue: $value, \ndata: ${data.toHexString()}\n}"
+    }
+}
+
+data class Spender(val address: Address)
+
