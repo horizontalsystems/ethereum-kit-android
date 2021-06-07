813e87d8b1b88b1d6b95d6c161fdf7d3507b9a56
abdrasulov <abdrasulov@gmail.com>
2021-06-07 13:44:40 +0600

    Add NetworkType Goerli

--- uniswapkit/src/main/java/io/horizontalsystems/uniswapkit/TokenFactory.kt
+++ uniswapkit/src/main/java/io/horizontalsystems/uniswapkit/TokenFactory.kt
@@ -19,10 +19,11 @@ class TokenFactory(networkType: NetworkType) {
         private fun wethAddress(networkType: NetworkType): Address {
             val wethAddressHex = when (networkType) {
                 NetworkType.EthMainNet -> "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-                NetworkType.EthRopsten -> "0xc778417e063141139fce010982780140aa0cd5ab"
-                NetworkType.EthKovan -> "0xd0A1E359811322d97991E03f863a0C30C2cF029C"
-                NetworkType.EthRinkeby -> "0xc778417e063141139fce010982780140aa0cd5ab"
                 NetworkType.BscMainNet -> "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c"
+                NetworkType.EthRopsten,
+                NetworkType.EthRinkeby -> "0xc778417E063141139Fce010982780140Aa0cD5Ab"
+                NetworkType.EthKovan -> "0xd0A1E359811322d97991E03f863a0C30C2cF029C"
+                NetworkType.EthGoerli -> "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6"
             }
             return Address(wethAddressHex)
         }
