d03c09717e7664f59b725bdd8c1a9dbef7eda168
chyngyz <chyngyz.omurov@gmail.com>
2021-07-09 15:34:26 +0600

    Migrate to PancakeSwap V2

--- uniswapkit/src/main/java/io/horizontalsystems/uniswapkit/TradeManager.kt
+++ uniswapkit/src/main/java/io/horizontalsystems/uniswapkit/TradeManager.kt
@@ -131,7 +131,7 @@ class TradeManager(
                     NetworkType.EthKovan,
                     NetworkType.EthGoerli,
                     NetworkType.EthRinkeby -> Address("0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D")
-                    NetworkType.BscMainNet -> Address("0x05fF2B0DB69458A0750badebc4f9e13aDd608C7F")
+                    NetworkType.BscMainNet -> Address("0x10ED43C718714eb63d5aA57B78B54704E256024E")
                 }
 
         fun tradeExactIn(pairs: List<Pair>, tokenAmountIn: TokenAmount, tokenOut: Token, maxHops: Int = 3, currentPairs: List<Pair> = listOf(), originalTokenAmountIn: TokenAmount? = null): List<Trade> {
