981d1ff7a040a1d603744928379232b063a46fb2
chyngyz <chyngyz.omurov@gmail.com>
2021-08-17 14:50:41 +0600

    Add 'nonce' to TransactionData

--- ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/TransactionData.kt
+++ ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/TransactionData.kt
@@ -7,12 +7,13 @@ import java.util.*
 data class TransactionData(
         val to: Address,
         val value: BigInteger,
-        val input: ByteArray
+        val input: ByteArray,
+        val nonce: Long? = null
 ) {
     override fun equals(other: Any?): Boolean {
         return when {
             this === other -> true
-            other is TransactionData -> to == other.to && value == other.value && input.contentEquals(other.input)
+            other is TransactionData -> to == other.to && value == other.value && input.contentEquals(other.input) && nonce == other.nonce
             else -> false
         }
     }
