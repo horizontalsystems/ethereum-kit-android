4c9912ee4d57d724c67f0a3fdddaf923b59eeac1
Rafael Muhamedzyanov <rafaelekol@mail.ru>
2021-12-28 12:24:59 +0600

    Remove Incubed client

--- ethereumkit/copy_in3_java_bindings.sh
+++
@@ -1,6 +0,0 @@
-#!/u
-
-# copy client to java path
-cp -r in3-c/java/src/in3 src/main/java/
-mkdir -p c
-cp in3-c/c/compiler.cmake c/
--- ethereumkit/src/main/java/in3/IN3Transport.java
+++
@@ -1,8 +0,0 @@
-package in3;
-
-/*
- * Interface for the IN3 transport.
- */
-interface IN3Transport {
-  byte[][] handle(String[] urls, byte[] payload);
-}
