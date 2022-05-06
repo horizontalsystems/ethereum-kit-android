b084b1c873dfd9a06d0e81d79ce262cfa70ebfa7
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2022-05-06 16:54:03 +0600

    Refactor RpcSource defaults

--- ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/RpcSource.kt
+++ ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/RpcSource.kt
@@ -7,56 +7,56 @@ sealed class RpcSource() {
     class WebSocket(val url: URL, val auth: String?) : RpcSource()
 
     companion object {
-        private fun infuraHttp(subdomain: String, projectId: String, projectSecret: String? = null): RpcSource {
-            return RpcSource.Http(listOf(URL("https://$subdomain.infura.io/v3/$projectId")), projectSecret)
+        private fun infuraHttp(subdomain: String, projectId: String, projectSecret: String? = null): Http {
+            return Http(listOf(URL("https://$subdomain.infura.io/v3/$projectId")), projectSecret)
         }
 
-        private fun infuraWebSocket(subdomain: String, projectId: String, projectSecret: String? = null): RpcSource {
-            return RpcSource.WebSocket(URL("https://$subdomain.infura.io/ws/v3/$projectId"), projectSecret)
+        private fun infuraWebSocket(subdomain: String, projectId: String, projectSecret: String? = null): WebSocket {
+            return WebSocket(URL("https://$subdomain.infura.io/ws/v3/$projectId"), projectSecret)
         }
 
-        fun ethereumInfuraHttp(projectId: String, projectSecret: String? = null): RpcSource {
+        fun ethereumInfuraHttp(projectId: String, projectSecret: String? = null): Http {
             return infuraHttp("mainnet", projectId, projectSecret)
         }
 
-        fun ropstenInfuraHttp(projectId: String, projectSecret: String? = null): RpcSource {
+        fun ropstenInfuraHttp(projectId: String, projectSecret: String? = null): Http {
             return infuraHttp("ropsten", projectId, projectSecret)
         }
 
-        fun kovanInfuraHttp(projectId: String, projectSecret: String? = null): RpcSource {
+        fun kovanInfuraHttp(projectId: String, projectSecret: String? = null): Http {
             return infuraHttp("kovan", projectId, projectSecret)
         }
 
-        fun rinkebyInfuraHttp(projectId: String, projectSecret: String? = null): RpcSource {
+        fun rinkebyInfuraHttp(projectId: String, projectSecret: String? = null): Http {
             return infuraHttp("rinkeby", projectId, projectSecret)
         }
 
-        fun goerliInfuraHttp(projectId: String, projectSecret: String? = null): RpcSource {
+        fun goerliInfuraHttp(projectId: String, projectSecret: String? = null): Http {
             return infuraHttp("goerli", projectId, projectSecret)
         }
 
-        fun ethereumInfuraWebSocket(projectId: String, projectSecret: String? = null): RpcSource {
+        fun ethereumInfuraWebSocket(projectId: String, projectSecret: String? = null): WebSocket {
             return infuraWebSocket("mainnet", projectId, projectSecret)
         }
 
-        fun ropstenInfuraWebSocket(projectId: String, projectSecret: String? = null): RpcSource {
+        fun ropstenInfuraWebSocket(projectId: String, projectSecret: String? = null): WebSocket {
             return infuraWebSocket("ropsten", projectId, projectSecret)
         }
 
-        fun kovanInfuraWebSocket(projectId: String, projectSecret: String? = null): RpcSource {
+        fun kovanInfuraWebSocket(projectId: String, projectSecret: String? = null): WebSocket {
             return infuraWebSocket("kovan", projectId, projectSecret)
         }
 
-        fun rinkebyInfuraWebSocket(projectId: String, projectSecret: String? = null): RpcSource {
+        fun rinkebyInfuraWebSocket(projectId: String, projectSecret: String? = null): WebSocket {
             return infuraWebSocket("rinkeby", projectId, projectSecret)
         }
 
-        fun goerliInfuraWebSocket(projectId: String, projectSecret: String? = null): RpcSource {
+        fun goerliInfuraWebSocket(projectId: String, projectSecret: String? = null): WebSocket {
             return infuraWebSocket("goerli", projectId, projectSecret)
         }
 
-        fun binanceSmartChainHttp(): RpcSource {
-            return RpcSource.Http(
+        fun binanceSmartChainHttp(): Http {
+            return Http(
                     listOf(
                             URL("https://bscrpc.com"),
                             URL("https://bsc-dataseed.binance.org/"),
@@ -77,20 +77,20 @@ sealed class RpcSource() {
             )
         }
 
-        fun binanceSmartChainWebSocket(): RpcSource {
-            return RpcSource.WebSocket(URL("https://bsc-ws-node.nariox.org:443"), null)
+        fun binanceSmartChainWebSocket(): WebSocket {
+            return WebSocket(URL("https://bsc-ws-node.nariox.org:443"), null)
         }
 
-        fun polygonRpcHttp(): RpcSource {
-            return RpcSource.Http(listOf(URL("https://polygon-rpc.com")), null)
+        fun polygonRpcHttp(): Http {
+            return Http(listOf(URL("https://polygon-rpc.com")), null)
         }
 
-        fun optimismRpcHttp(): RpcSource {
-            return RpcSource.Http(listOf(URL("https://mainnet.optimism.io")), null)
+        fun optimismRpcHttp(): Http {
+            return Http(listOf(URL("https://mainnet.optimism.io")), null)
         }
 
-        fun arbitrumOneRpcHttp(): RpcSource {
-            return RpcSource.Http(listOf(URL("https://arb1.arbitrum.io/rpc")), null)
+        fun arbitrumOneRpcHttp(): Http {
+            return Http(listOf(URL("https://arb1.arbitrum.io/rpc")), null)
         }
 
     }
