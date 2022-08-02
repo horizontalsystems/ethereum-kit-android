d89cd9f1c350bc7704cceae75b6eb75e912f920d
Kydyr uulu Esenbek <esenbekkd@gmail.com>
2022-08-02 17:16:53 +0600

    Add L1FeeProvider for rollups with smart contract who can provide L1 fee

---
+++ ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/core/rollup/L1FeeProvider.kt
@@ -0,0 +1,32 @@
+package io.horizontalsystems.ethereumkit.core.rollup
+
+import io.horizontalsystems.ethereumkit.contracts.ContractMethod
+import io.horizontalsystems.ethereumkit.core.EthereumKit
+import io.horizontalsystems.ethereumkit.core.TransactionBuilder
+import io.horizontalsystems.ethereumkit.models.Address
+import io.horizontalsystems.ethereumkit.models.GasPrice
+import io.horizontalsystems.ethereumkit.models.RawTransaction
+import io.horizontalsystems.ethereumkit.spv.core.toBigInteger
+import io.reactivex.Single
+import java.math.BigInteger
+
+class L1FeeProvider(
+        private val evmKit: EthereumKit,
+        private val contractAddress: Address
+) {
+
+    class L1FeeMethod(val transaction: ByteArray) : ContractMethod() {
+        override val methodSignature = "getL1Fee(bytes)"
+        override fun getArguments() = listOf(transaction)
+    }
+
+    fun getL1Fee(gasPrice: GasPrice, gasLimit: Long, to: Address, value: BigInteger, data: ByteArray, nonce: Long?): Single<BigInteger> {
+        val rawTransaction = RawTransaction(gasPrice, gasLimit, to, value, nonce ?: 1, data)
+        val encoded = TransactionBuilder.encode(rawTransaction, null, evmKit.chain.id)
+        val data = L1FeeMethod(encoded).encodedABI()
+
+        return evmKit.call(contractAddress, data)
+                .map { it.sliceArray(IntRange(0, 31)).toBigInteger() }
+    }
+
+}
