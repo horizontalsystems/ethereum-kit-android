511a1e66811f6586ddef9b97f549f451567518ae
chyngyz <chyngyz.omurov@gmail.com>
2022-08-12 18:34:18 +0600

    Make `Chain` enum class

--- ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/Chain.kt
+++ ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/models/Chain.kt
@@ -1,22 +1,21 @@
 package io.horizontalsystems.ethereumkit.models
 
-sealed class Chain(
-    val id: Int,
-    val coinType: Int,
-    val syncInterval: Long,
-    val isEIP1559Supported: Boolean
+enum class Chain(
+        val id: Int,
+        val coinType: Int,
+        val syncInterval: Long,
+        val isEIP1559Supported: Boolean
 ) {
+    Ethereum(1, 60, 15, true),
+    BinanceSmartChain(56, 60, 15, false),
+    Polygon(137, 60, 15, true),
+    Optimism(10, 60, 15, false),
+    ArbitrumOne(42161, 60, 15, false),
+    Avalanche(43114, 60, 15, true),
+    EthereumRopsten(3, 1, 15, true),
+    EthereumKovan(42, 1, 15, true),
+    EthereumRinkeby(4, 1, 15, true),
+    EthereumGoerli(5, 1, 15, true);
+
     val isMainNet = coinType != 1
-
-    object Ethereum: Chain(1, 60, 15, true)
-    object BinanceSmartChain: Chain(56, 60, 15, false)
-    object Polygon: Chain(137, 60, 15, true)
-    object Optimism: Chain(10, 60, 15, false)
-    object ArbitrumOne: Chain(42161, 60, 15, false)
-    object Avalanche: Chain(43114, 60, 15, true)
-    object EthereumRopsten: Chain(3, 1, 15, true)
-    object EthereumKovan: Chain(42, 1, 15, true)
-    object EthereumRinkeby: Chain(4, 1, 15, true)
-    object EthereumGoerli: Chain(5, 1, 15, true)
-
 }
