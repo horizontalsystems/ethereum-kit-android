0bc6d9bb221bd153c8d514aa8692e293d24a2ec8
avenbreaks <dev-cosmware@gitshock.com>
2022-11-22 17:08:58 +0800

    Adding New Chain

--- uniswapkit/src/main/java/io/horizontalsystems/uniswapkit/TokenFactory.kt
+++ uniswapkit/src/main/java/io/horizontalsystems/uniswapkit/TokenFactory.kt
@@ -25,6 +25,7 @@ class TokenFactory(chain: Chain) {
                 Chain.Ethereum -> "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
                 Chain.Optimism -> "0x4200000000000000000000000000000000000006"
                 Chain.BinanceSmartChain -> "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c"
+                Chain.Gitshock -> "0x9805275fB2f4d75cb1f55ff8ca13541124b7a52B"
                 Chain.Polygon -> "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270"
                 Chain.Avalanche -> "0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7"
                 Chain.EthereumGoerli -> "0xB4FBF271143F4FBf7B91A5ded31805e42b2208d6"
