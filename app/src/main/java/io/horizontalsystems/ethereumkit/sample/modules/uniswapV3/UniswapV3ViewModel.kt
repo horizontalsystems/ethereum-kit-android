9b84bcbe250fccd2be4848152f703c1a461a9ac4
abdrasulov <abdrasulov@gmail.com>
2023-04-11 15:09:32 +0600

    Add sample module for Uniswap v3

---
+++ app/src/main/java/io/horizontalsystems/ethereumkit/sample/modules/uniswapV3/UniswapV3ViewModel.kt
@@ -0,0 +1,203 @@
+package io.horizontalsystems.ethereumkit.sample.modules.uniswapV3
+
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import io.horizontalsystems.ethereumkit.core.EthereumKit
+import io.horizontalsystems.ethereumkit.core.signer.Signer
+import io.horizontalsystems.ethereumkit.core.toHexString
+import io.horizontalsystems.ethereumkit.models.GasPrice
+import io.horizontalsystems.ethereumkit.sample.Configuration
+import io.horizontalsystems.ethereumkit.sample.core.Erc20Adapter
+import io.horizontalsystems.ethereumkit.sample.modules.main.Erc20Token
+import io.horizontalsystems.ethereumkit.sample.modules.main.GasPriceHelper
+import io.horizontalsystems.uniswapkit.UniswapV3Kit
+import io.horizontalsystems.uniswapkit.models.TradeOptions
+import io.horizontalsystems.uniswapkit.models.TradeType
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.reactive.collect
+import kotlinx.coroutines.rx2.await
+import java.math.BigDecimal
+
+class UniswapV3ViewModel(
+    private val ethereumKit: EthereumKit,
+    private val erc20Adapter: Erc20Adapter,
+    private val gasPriceHelper: GasPriceHelper,
+    private val signer: Signer
+) : ViewModel() {
+
+    val fromToken: Erc20Token = Configuration.erc20Tokens[0]
+    val toToken: Erc20Token = Configuration.erc20Tokens[1]
+
+    private var uniswapV3Kit = UniswapV3Kit.getInstance(ethereumKit)
+
+    var swapState by mutableStateOf<SwapState?>(null)
+        private set
+    private var gasPrice: GasPrice = GasPrice.Legacy(20_000_000_000)
+
+    val swapStatus = mutableStateOf<Throwable?>(null)
+
+    private val tradeOptions = TradeOptions(allowedSlippagePercent = BigDecimal("0.5"))
+
+    init {
+        viewModelScope.launch {
+            gasPriceHelper.gasPriceFlowable().collect {
+                gasPrice = it
+            }
+        }
+    }
+
+    fun syncAllowance() {
+        viewModelScope.launch {
+            try {
+                val it = erc20Adapter.allowance(uniswapV3Kit.routerAddress).await()
+                Log.e("AAA", "allowance: ${it.toPlainString()}")
+            } catch (it: Throwable) {
+                Log.e("AAA", "swapData ERROR = ${it.message}")
+            }
+        }
+    }
+
+    fun approve(decimalAmount: BigDecimal) {
+        val spenderAddress = uniswapV3Kit.routerAddress
+
+        val token = fromToken
+        val amount = decimalAmount.movePointRight(token.decimals).toBigInteger()
+
+        val transactionData = erc20Adapter.approveTransactionData(spenderAddress, amount)
+
+        viewModelScope.launch {
+            try {
+                val gasLimit = ethereumKit.estimateGas(transactionData, gasPrice).await()
+                Log.e("AAA", "gas limit: $gasLimit")
+                val rawTransaction = ethereumKit.rawTransaction(transactionData, gasPrice, gasLimit).await()
+                val signature = signer.signature(rawTransaction)
+                val fullTransaction = ethereumKit.send(rawTransaction, signature).await()
+                Log.e("AAA", "approve: ${fullTransaction.transaction.hash}")
+
+            } catch (it: Throwable) {
+                Log.e("AAA", "approve ERROR = ${it.message}")
+            }
+        }
+    }
+
+    fun onChangeAmountIn(amountIn: BigDecimal?) {
+        if (amountIn == null) {
+            swapState = SwapState(
+                amountIn = null,
+                amountOut = null,
+                tradeType = TradeType.ExactIn
+            )
+            return
+        }
+
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val it = uniswapV3Kit
+                    .bestTradeExactIn(
+                        tokenIn = fromToken.contractAddress,
+                        tokenOut = toToken.contractAddress,
+                        amountIn = amountIn.multiply(BigDecimal(10).pow(fromToken.decimals)).toBigInteger()
+                    )
+                    .await()
+                val amountOut = it.toBigDecimal().divide(BigDecimal(10).pow(toToken.decimals))
+                swapState = SwapState(
+                    amountIn = amountIn,
+                    amountOut = amountOut,
+                    tradeType = TradeType.ExactIn
+                )
+            } catch (error: Throwable) {
+                Log.e("AAA", "bestTradeExactIn error: ${error.javaClass.simpleName} (${error.localizedMessage})")
+            }
+        }
+    }
+
+    fun onChangeAmountOut(amountOut: BigDecimal?) {
+        if (amountOut == null) {
+            swapState = SwapState(
+                amountIn = null,
+                amountOut = null,
+                tradeType = TradeType.ExactOut
+            )
+            return
+        }
+
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val it = uniswapV3Kit
+                    .bestTradeExactOut(
+                        tokenIn = fromToken.contractAddress,
+                        tokenOut = toToken.contractAddress,
+                        amountOut = amountOut.multiply(BigDecimal(10).pow(toToken.decimals)).toBigInteger()
+                    )
+                    .await()
+
+                val amountIn = it.toBigDecimal().divide(BigDecimal(10).pow(fromToken.decimals))
+                swapState = SwapState(
+                    amountIn = amountIn,
+                    amountOut = amountOut,
+                    tradeType = TradeType.ExactOut
+                )
+            } catch (error: Throwable) {
+                Log.e("AAA", "bestTradeExactOut error: ${error.javaClass.simpleName} (${error.localizedMessage})")
+            }
+        }
+    }
+
+    fun swap() {
+        val swapState = swapState ?: return
+        val amountInDecimal = swapState.amountIn ?: return
+        val amountOutDecimal = swapState.amountOut ?: return
+
+        viewModelScope.launch {
+            val amountIn = amountInDecimal.multiply(BigDecimal(10).pow(fromToken.decimals)).toBigInteger()
+            val amountOut = amountOutDecimal.multiply(BigDecimal(10).pow(toToken.decimals)).toBigInteger()
+            val transactionData = uniswapV3Kit.transactionData(
+                tradeType = swapState.tradeType,
+                tokenIn = fromToken.contractAddress,
+                tokenOut = toToken.contractAddress,
+                amountIn = amountIn,
+                amountOut = amountOut,
+                tradeOptions = tradeOptions
+            )
+
+            try {
+                val gasLimit = ethereumKit.estimateGas(transactionData, gasPrice).await()
+                Log.e("AAA", "gas limit: $gasLimit")
+                val rawTransaction = ethereumKit.rawTransaction(transactionData, gasPrice, gasLimit).await()
+                val signature = signer.signature(rawTransaction)
+                val fullTransaction = ethereumKit.send(rawTransaction, signature).await()
+                swapStatus.value = null
+                Log.e("AAA", "swap SUCCESS, txHash=${fullTransaction.transaction.hash.toHexString()}")
+            } catch (it: Throwable) {
+                swapStatus.value = it
+                Log.e("AAA", "swap ERROR, error=${it.message}")
+                it.printStackTrace()
+            }
+        }
+    }
+
+    class Factory(
+        private val ethereumKit: EthereumKit,
+        private val erc20Adapter: Erc20Adapter,
+        private val gasPriceHelper: GasPriceHelper,
+        private val signer: Signer
+    ) : ViewModelProvider.Factory {
+        @Suppress("UNCHECKED_CAST")
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return UniswapV3ViewModel(ethereumKit, erc20Adapter, gasPriceHelper, signer) as T
+        }
+    }
+}
+
+
+data class SwapState(
+    val amountOut: BigDecimal?,
+    val amountIn: BigDecimal?,
+    val tradeType: TradeType
+)
