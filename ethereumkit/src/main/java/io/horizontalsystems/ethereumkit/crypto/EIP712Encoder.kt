729ee3fceeeb9fb89701e9930f2191d3a3af4065
chyngyz <chyngyz.omurov@gmail.com>
2023-06-09 16:44:26 +0600

    Implement Eip712 message sanitization

--- ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/crypto/EIP712Encoder.kt
+++ ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/crypto/EIP712Encoder.kt
@@ -1,6 +1,9 @@
 package io.horizontalsystems.ethereumkit.crypto
 
 import com.google.gson.Gson
+import com.google.gson.JsonArray
+import com.google.gson.JsonElement
+import com.google.gson.JsonObject
 import org.web3j.crypto.StructuredDataEncoder
 
 class EIP712Encoder {
@@ -20,13 +23,41 @@ class EIP712Encoder {
 }
 
 data class TypedData(
-        val types: Map<String, List<TypeParam>>,
-        val primaryType: String,
-        val domain: Map<String, Any>,
-        val message: Map<String, Any>
-)
+    val types: Map<String, List<TypeParam>>,
+    val primaryType: String,
+    val domain: Map<String, Any>,
+    val message: JsonElement
+) {
+    val sanitizedMessage: String
+        get() = sanitized(message, primaryType).toString()
+
+    private fun sanitized(json: JsonElement, type: String): JsonElement = when (json) {
+        is JsonObject -> {
+            val sanitizedObject = JsonObject()
+
+            for ((key, value) in json.entrySet()) {
+                val currentTypes = types[type] ?: continue
+                val currentType = currentTypes.find { it.name == key } ?: continue
+                sanitizedObject.add(key, sanitized(value, currentType.type))
+            }
+            sanitizedObject
+        }
+
+        is JsonArray -> {
+            val sanitizedArray = json.map { sanitized(it, type.replace("[", "").replace("]", "")) }
+
+            val jsonArray = JsonArray(sanitizedArray.size)
+            sanitizedArray.forEach { jsonArray.add(it) }
+            jsonArray
+        }
+
+        else -> {
+            json
+        }
+    }
+}
 
 data class TypeParam(
-        val name: String,
-        val type: String
+    val name: String,
+    val type: String
 )
