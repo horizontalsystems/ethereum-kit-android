af794e01ee5ffbe7bf5272035eb1cd21f2737151
Rafael <rafaelekol@gmail.com>
2025-06-20 15:14:55 +0600

    Catch method parser exceptions

--- ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/contracts/ContractMethodFactories.kt
+++ ethereumkit/src/main/java/io/horizontalsystems/ethereumkit/contracts/ContractMethodFactories.kt
@@ -25,7 +25,11 @@ open class ContractMethodFactories {
 
         val methodFactory = methodFactories[methodId.toInt()]
 
-        return methodFactory?.createMethod(input.copyOfRange(4, input.size))
+        return try {
+            methodFactory?.createMethod(input.copyOfRange(4, input.size))
+        } catch (e: IndexOutOfBoundsException) {
+            null
+        }
     }
 
 }
